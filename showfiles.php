57f66bd133b1890528b94f98ed588ebdda9e82a5
root <root@demo-apache.brightprocess.com>
2014-01-17 16:19:43 +0000

    fixed student portal

--- showfiles.php
+++ showfiles.php
@@ -1,18 +1,17 @@
 <div class="row">
 <?php 
-
-// open this directory 
-$dir = opendir("server/php/files");
-
-// get each entry
-while($file = readdir($dir)) {
-    if (substr($file, 0, 1) != '.') {
-        echo '<div class="col-xs-6 col-md-3"><a href="#" class="thumbnail"><img src="server/php/files/' . $file . '"/></a></div>';
-    }
+$dir = "server/php/files/"; 
+chdir($dir); 
+array_multisort(array_map('filemtime', ($files = glob("*.*"))), SORT_DESC, $files); 
+foreach($files as $filename) { 
+$file_type = pathinfo($filename, PATHINFO_EXTENSION);
+if($file_type == "png" || $file_type == "jpeg" || $file_type == "jpg" || $file_type == "gif"){
+echo '<div class="col-xs-6 col-md-3"><a href="#" class="thumbnail"><img src="server/php/files/' . $filename . '"/></a></div>';
+} else{ 
+if($file_type == "sql"){
+echo '<div class="col-xs-6 col-md-3"><a href="#" class="thumbnail"><img src="http://l1311.hizliresim.com/1h/g/ujrjn.png"/></a></div>';
 }
-
-// close directory
-closedir($dir);
-
+}
+}  
 ?>
 </div>
